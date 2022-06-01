64919add5ce2777d833889939509f88d81786058
Kibertex <Kibertex@DESKTOP-I2KH5HE>
2022-06-01 17:07:51 +0200

    Add project files.

---
+++ hangman/hangman.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+using namespace std;
+
+void PrintMessage(string message, bool printTop = true, bool printBottom = true)
+{
+	if (printTop)
+	{
+		cout << "+---------------------------------+" << endl;
+		cout << "|";
+	}
+	else
+	{
+		cout << "|";
+	}
+	bool front = true;
+	for (int i = message.length(); i < 33; i++)
+	{
+		if (front)
+		{
+			message = " " + message;
+		}
+		else 
+		{
+			message = message + " ";
+		}
+		front = !front;
+
+	}
+	cout << message.c_str();
+
+	if (printBottom)
+	{
+		cout << "|" << endl;
+		cout << "+---------------------------------+" << endl;
+	}
+	else
+	{
+		cout << "|" << endl;
+	}
+}
+void DrawHangman(int guessCount = 0)
+{
+	if (guessCount >= 1)
+		PrintMessage("|", false, false);
+	else
+		PrintMessage("", false, false);
+
+	if (guessCount >= 2)
+		PrintMessage("|", false, false);
+	else
+		PrintMessage("", false, false);
+
+	if (guessCount >= 3)
+		PrintMessage("0", false, false);
+	else
+		PrintMessage("", false, false);
+
+	if (guessCount == 4)
+		PrintMessage("/", false, false);
+	else
+		PrintMessage("", false, false);
+
+	if (guessCount == 5)
+		PrintMessage("/|", false, false);
+	else
+
+	if (guessCount >= 6)
+		PrintMessage("/|\\", false, false);
+	else
+		PrintMessage("", false, false);
+
+	if (guessCount >= 7)
+		PrintMessage("|", false, false);
+	else
+		PrintMessage("", false, false);
+
+	if (guessCount == 8)
+		PrintMessage("/", false, false);
+
+	if (guessCount >= 9)
+		PrintMessage("/ \\", false, false);
+	else
+		PrintMessage("", false, false);
+	
+}
+void PrintLetters(string input, char from, char to)
+{
+	string s;
+	for (char i = from; i <= to; i++)
+	{
+		if (input.find(i) == string::npos)
+			s += i;
+			s += i + " ";
+		else
+			s += " ";
+	}
+	PrintMessage(s, false, false);
+}
+void PrintAvailableletters(string taken)
+{
+	PrintLetters(taken, 'A', 'M');
+	PrintLetters(taken, 'N', 'Z');
+}
+int main()
+{
+	string guesses;
+	PrintMessage("HANG MAN");
+	DrawHangman(9);
+	PrintAvailableletters("ALEXA");
+	getchar();
+	return 0;
+}
+
+/*
++---------------------------------+
+|             HANG MAN            |
++---------------------------------+
+|                |                |
+|                |                |
+|                0                |
+|               /|\               |
+|                |                |
+|               / \               |
+|          +-----------+          |
+|          |           |          |
++---------------------------------+
+|        Available letters        |
++---------------------------------+
+|    A B C D E F G H I J K L M    |
+|    N O P Q R S T U V W X Y Z    |
+|         Guess the word          |
++---------------------------------+
+|  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  |
++---------------------------------|
+*/
